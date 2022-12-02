6a3b53c6d6a306196244146bf552d8d92908404d
Rafi Ferdos <76879162+rafiferdos@users.noreply.github.com>
2022-12-02 13:06:03 +0600

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    int num;
+    scanf("%d", &num);
+    char questions[100];
+        gets(questions);
+        printf("%s", questions);
+    return 0;
+}
